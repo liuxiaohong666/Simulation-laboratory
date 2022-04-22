f46a7a463c445a690a6815e16afd8347372e7b63
liuxiaohong666 <103727717+liuxiaohong666@users.noreply.github.com>
2022-04-22 19:50:49 +0800

    Add files via upload

---
+++ signIn.java
@@ -0,0 +1,15 @@
+package com.example.booksystem;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksystemApplication {
+	
+    public static void main(String[] args) {
+    	//启动sping项目
+        SpringApplication.run(BooksystemApplication.class, args);
+    }
+
+}
